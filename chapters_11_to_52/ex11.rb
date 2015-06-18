5b1b733cdddf5e669afefe759116c3445f2b20d9
Rebecca <rfjackson11@gmail.com>
2015-06-17 18:35:32 -0600

    Add Chapter 11
    [#95933714]

---
+++ chapters_11_to_52/ex11.rb
@@ -0,0 +1,18 @@
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+puts
+
+print "What is your first name? "
+first_name = gets.chomp.capitalize
+print "What is your middle name? "
+middle_name = gets.chomp.capitalize
+print "What is your last name? "
+last_name = gets.chomp.capitalize
+
+puts "Your full name is  #{first_name} #{middle_name} #{last_name}."
